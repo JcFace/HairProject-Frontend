24544af3be2a1974edd2d01cfb1bc94b39da98cf
JcFace <jcv@gmail.com>
2020-12-02 21:36:16 -0800

    Second commit

--- src/index.js
+++ src/index.js
@@ -5,12 +5,24 @@
     function getHairTypes() {
         fetch(hairTypesUrl)
         .then(res => res.json())
-        .then(hair => showHair(hair))
+        .then(types => types.forEach(type => showType(type)))
     }
     getHairTypes()
 
-    function showHair(hair) {
-        
+    function showType(type) {
+        const div = document.createElement('div')
+        const h2 = document.createElement('h2')
+        const typeImage = document.createElement('img')
+
+        typeImage.src = type.img_url
+
+        div.id = type.id
+
+        h2.innerText = type.name
+
+        div.append(h2, typeImage)
+        main.appendChild(div)
+
     }
 
 })()
